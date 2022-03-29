e6ae67a815c1ba440f6bec9d31b9e482cdf12273
Aziza Rakhmonova <arakhmonova@foundationmedicine.com>
2022-03-29 19:33:34 -0400

    employee registration project

---
+++ src/main/java/com/cydeo/EmployeeRegistrationSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationSpringProjectApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (EmployeeRegistrationSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/employeeregistrationspringproject/EmployeeRegistrationSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistrationspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
